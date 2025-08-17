14675f361510acc0c9895a6af0301f8992fea3aa
Elaine Ngunu <elainewaithere90@gmail.com>
2025-08-17 13:11:38 +0300

    fix: Final version of task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,1 @@
--- This script prints the full description of the table 'books'
--- from the specified database without using DESCRIBE or EXPLAIN.
-
-SHOW CREATE TABLE `books`;
+SHOW CREATE TABLE `books`\G
