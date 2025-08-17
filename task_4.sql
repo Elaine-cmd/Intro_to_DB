2d5c5b8d4ae18c1ecd5e4b066c14612b326c4194
Elaine Ngunu <elainewaithere90@gmail.com>
2025-08-17 12:59:51 +0300

    fix: Replace task_4.sql with correct version

--- task_4.sql
+++ task_4.sql
@@ -2,12 +2,12 @@
 -- from the specified database without using DESCRIBE or EXPLAIN.
 
 SELECT
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
+    COLUMN_NAME AS `Field`,
+    COLUMN_TYPE AS `Type`,
+    IS_NULLABLE AS `Null`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
 FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE
