3d474f005edb6d66652c2283000da883d2c2538b
Elaine Ngunu <elainewaithere90@gmail.com>
2025-08-17 12:58:04 +0300

    fix: Replace task_4.sql with correct version

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,14 @@
 -- This script prints the full description of the table 'books'
 -- from the specified database without using DESCRIBE or EXPLAIN.
 
-SHOW COLUMNS FROM books;
+SELECT
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
