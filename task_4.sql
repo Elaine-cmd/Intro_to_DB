298a1b5c4ba58adad1a62328b585e3bb28a0179e
Elaine Ngunu <elainewaithere90@gmail.com>
2025-08-17 12:44:05 +0300

    feat: Add script to show table description

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,4 @@
 -- This script prints the full description of the table 'books'
 -- from the specified database without using DESCRIBE or EXPLAIN.
 
-SELECT
-    COLUMN_NAME AS `Field`,
-    COLUMN_TYPE AS `Type`,
-    IS_NULLABLE AS `Null`,
-    COLUMN_KEY AS `Key`,
-    COLUMN_DEFAULT AS `Default`,
-    EXTRA AS `Extra`
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
+SHOW COLUMNS FROM books;
